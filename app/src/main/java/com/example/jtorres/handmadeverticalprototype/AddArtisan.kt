04ee973d3973a83e108f62b2830e6dc76112c8b8
John Torres <jandrewtorres5@gmail.com>
2018-11-15 12:47:23 -0800

    Initial commit

---
+++ app/src/main/java/com/example/jtorres/handmadeverticalprototype/AddArtisan.kt
@@ -0,0 +1,12 @@
+package com.example.jtorres.handmadeverticalprototype
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddArtisan : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_artisan)
+    }
+}
