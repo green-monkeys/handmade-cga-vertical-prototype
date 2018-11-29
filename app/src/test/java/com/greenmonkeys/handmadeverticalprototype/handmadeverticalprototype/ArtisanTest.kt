63db256254573c67bc25ebb099e1e99d3c4967be
elammend <eddy.lam97@gmail.com>
2018-11-28 17:34:49 -0800

    2 more tests

--- app/src/test/java/com/greenmonkeys/handmadeverticalprototype/handmadeverticalprototype/ArtisanTest.kt
+++ app/src/test/java/com/greenmonkeys/handmadeverticalprototype/handmadeverticalprototype/ArtisanTest.kt
@@ -1,6 +1,7 @@
 package com.greenmonkeys.handmadeverticalprototype.handmadeverticalprototype
 
 import com.greenmonkeys.verticalprototype.model.Artisan
+import com.greenmonkeys.verticalprototype.model.ArtisanContainer
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -11,4 +12,24 @@ class ArtisanTest {
         val artisan = Artisan("John", "Doe", "email@mail.com")
         assertEquals( "John Doe", artisan.toString())
     }
+
+    var tempContainer = ArtisanContainer
+    var artisan1 = Artisan("Edward","Lam","eddieSpam@email.com")
+    var artisan2 = Artisan("Blue","Steel","zooMan@email.com")
+    var artisan3 = Artisan("Grey","Fox","jaeger@email.com")
+
+    @Test
+    fun addSingleArtisan() {
+        tempContainer.addArtisan(artisan1)
+        assertTrue(tempContainer.artisans.contains(artisan1))
+    }
+
+    @Test
+    fun addArtisan() {
+        tempContainer.addArtisan(artisan2)
+        assertTrue(tempContainer.artisans.contains(artisan2))
+        tempContainer.addArtisan(artisan3)
+        assertTrue(tempContainer.artisans.contains(artisan3))
+    }
+    
 }
